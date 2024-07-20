4dbea168650f6283bfe1f67f8af89d04e8b841f7
vaishnavi-3969 <vaishnavi.kale3011@gmail.com>
2024-07-20 21:41:11 +0530

    basic folder structure initialized

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Home, Landing, Profile } from './pages';
 
 function App() {
+  const routes = [
+    { path: '/', element: <Landing /> },
+    { path: '/home', element: <Home /> },
+    { path: '/profile', element: <Profile /> },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {
+            routes.map(route => (
+              <Route
+                path={route.path}
+                element={route.element}
+                exact
+              />
+            ))
+          }
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/index.js
@@ -0,0 +1,11 @@
+import About from "./About";
+import Home from "./Home";
+import Landing from "./Landing";
+import Profile from "./Profile";
+
+export {
+    About,
+    Home,
+    Landing,
+    Profile
+}
