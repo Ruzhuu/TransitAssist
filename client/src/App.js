d4c31715c3d10835acac7ad6889699fa5e68ed22
vaishnavi-3969 <vaishnavi.kale3011@gmail.com>
2024-07-22 21:35:27 +0530

    auth0 added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home, Landing, Profile } from './pages';
+import { useAuth0 } from '@auth0/auth0-react';
+import Navbar from './components/Navbar';
+import { Footer } from './components';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   const routes = [
-    { path: '/', element: <Landing /> },
-    { path: '/home', element: <Home /> },
+    { path: '/', element: isAuthenticated ? <Home /> : <Landing /> },
     { path: '/profile', element: <Profile /> },
   ]
   return (
     <div>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           {
             routes.map(route => (
@@ -22,6 +27,7 @@ function App() {
             ))
           }
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
   return (
-    <div>Navbar</div>
+
+    <div>
+      {
+        isAuthenticated ? (
+          <div>
+            <button
+              className='p-3 m-3 bg-green-600'
+              onClick={() => logout({ returnTo: window.location.origin })}
+            >
+              Log Out
+            </button>
+          </div>
+        ) : (
+          <button
+            className='p-3 m-3 bg-green-600'
+            onClick={() => loginWithRedirect()}
+          >
+            Log In
+          </button>
+        )
+      }
+    </div>
   )
 }
 
